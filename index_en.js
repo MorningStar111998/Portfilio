bab260531c87c39d46b7696ab057acabc2ae36f7
Regis TOUGOURI <regisprofessionel@gmail.com>
2023-11-06 15:53:53 +0100

    scroll pour faire apparaitre section et corriger double clique drapeau

--- index_en.js
+++ index_en.js
@@ -1,15 +1,17 @@
+
+
 document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("englishFlag")
     .addEventListener("click", function (event) {
-      event.preventDefault();
+      // event.preventDefault();
       toggleDropdown();
     });
 
   function toggleDropdown() {
     const dropdown = document.querySelector(".dropdown");
 
-    if (dropdown.style.top === "0px") {
+    if (dropdown.style.top === "0px" || dropdown.style.top==="") {
       dropdown.style.top = "100%";
       setTimeout(function () {
         dropdown.style.zIndex = "1";
@@ -19,4 +21,44 @@ document.addEventListener("DOMContentLoaded", function () {
       dropdown.style.top = "0";
     }
   }
+});
+
+
+const faders = document.querySelectorAll(".fade-in");
+options = {
+  threshold: 0.2,
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+options);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+$(document).ready(function () {
+  $(".contact-me").click(function (event) {
+    event.preventDefault();
+    var target = $($(this).attr("href"));
+    if (target.length) {
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top,
+        },
+        700
+      ); // You can adjust the duration for a faster or slower scroll
+    }
+  });
 });
